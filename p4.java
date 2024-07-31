64b6183a6a8ecf1b4e3ca76fb8c0d99c45758a9c
whoogeek <rlarjswn0303@naver.com>
2024-07-31 17:02:33 +0900

    test1: Java 기본 과제

---
+++ p4.java
@@ -0,0 +1,23 @@
+package com.whoo;
+
+public class p4 {
+
+    public static void main(String[] args) {
+
+        double num1 = 80.5;
+        double num2 = 50.6;
+        double num3 = 70.8;
+
+        System.out.println("총점 : " + (int)(num1 + num2 + num3));
+        System.out.println("평균 : " + (int)((num1 + num2 + num3)/3));
+        /* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
+         * 총점과 평균을 정수 형태로 출력하세요
+         *
+         * -- 출력 예시 --
+         * 총점 : 201
+         * 평균 : 67
+         *  */
+
+    }
+
+}
