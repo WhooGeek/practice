94612f6d991a8141734a00aaa88924db58d4d1c9
whoogeek <rlarjswn0303@naver.com>
2024-08-09 17:40:27 +0900

    upload: 08-09 과제 업로드

--- class1/chap12/Application01.java
+++ class1/chap12/Application01.java
@@ -1,2 +1,21 @@
-package com.whoo.chap12;public class Application01 {
+package com.whoo.chap12;
+
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        System.out.println("swap 전 : " + Arrays.toString(intArray));  // [1, 2, 3, 4, 5]
+        swap(intArray, 1, 3);
+        System.out.println("swap 후 : " + Arrays.toString(intArray));  // [1, 4, 3, 2, 5]
+
+        String[] strArray = {"A", "B", "C", "D"};
+        System.out.println("swap 전 : " + Arrays.toString(strArray));  // [A, B, C, D]
+        swap(strArray, 0, 2);
+        System.out.println("swap 후 : " + Arrays.toString(strArray));  // [C, B, A, D]
+    }
+
 }
